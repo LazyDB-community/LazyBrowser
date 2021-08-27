8910ff8fd3ee87ba2f312eb905944e1890da93ec
MoskalykA <45717749+MoskalykA@users.noreply.github.com>
2021-08-28 00:50:46 +0200

    New Syntax, Add minifed version

--- lazydb.js
+++ lazydb.js
@@ -1,263 +1,260 @@
-// users/uid/c = 200 (connected) 300 (AFK) 400 (disconnected)
-// values of w = true (disk) false (event only)
-
 class Database {
-    constructor(addr, port, onconnect = (e) => console.log('LazyDB is ready!'), onclose = (e) => console.log('LazyDB server lost!'), secure = true) {
-        this.addr = addr
-        this.port = port
-        this.id = 0
+    constructor(addr, port, onconnect = (e) => console.log("LazyDB is ready!"), onclose = (e) => console.log("LazyDB server lost!"), secure = true) {
+        this.addr = addr;
+        this.port = port;
+        this.id = 0;
 
-        this.lazy_sep = "\t\n\'lazy_sep\'\t\n"
+        this.lazy_sep = "\t\n\"lazy_sep\"\t\n";
 
-        this.callbacks = {}
-        this.messageQueue = []
+        this.callbacks = {};
+        this.messageQueue = [];
 
-        let sec = ''
+        let sec = "";
         if(secure) {
-            sec += 's'
+            sec += "s";
         }
 
-        this.ws = new WebSocket(`ws${sec}://${addr}:${port}`)
-        this.ws.onopen = e => onconnect(e)
-        this.ws.onclose = e => onclose(e)
+        this.ws = new WebSocket(`ws${sec}://${addr}:${port}`);
+        this.ws.onopen = e => onconnect(e);
+        this.ws.onclose = e => onclose(e);
         this.ws.onmessage = e => {
-            let messages = e.data.split('|')
+            let messages = e.data.split("|")
 
             for (let i = 0; i < messages.length; i++) {
-                const msg = messages[i]
-                let data = this.messageToJavascript(msg)
+                const msg = messages[i];
+                let data = this.messageToJavascript(msg);
                 if (data.s) {
-                    this.callbacks[data.id].resolve(data.r)
+                    this.callbacks[data.id].resolve(data.r);
                 } else {
-                    this.callbacks[data.id].reject(data.r)
+                    this.callbacks[data.id].reject(data.r);
                 }
 
                 if(!this.callbacks[data.id].sync) {
-                    delete this.callbacks[data.id]
+                    delete this.callbacks[data.id];
                 } else {
-                    this.callbacks[data.id].sync(data.r)
+                    this.callbacks[data.id].sync(data.r);
                 }
             }
-        }
+        };
 
         this.sendQueue = function (self) {
             if(self.messageQueue.length > 0 && (self.ws.readyState === WebSocket.OPEN)) {
-                self.ws.send(self.messageQueue.join('|'))
+                self.ws.send(self.messageQueue.join("|"));
 
-                self.messageQueue = []
+                self.messageQueue = [];
             }
         }
 
-        setInterval(this.sendQueue, 100, this)
-    }
+        setInterval(this.sendQueue, 100, this);
+    };
 
     messageToJavascript(msg) {
-         return JSON.parse(msg.replaceAll(this.lazy_sep, '|'))
-    }
+        return JSON.parse(msg.replaceAll(this.lazy_sep, "|"));
+    };
 
     javascriptToMessage(data) {
-        const msg = JSON.stringify(data)
-        return msg.replaceAll('|',this.lazy_sep)
-    }
+        const msg = JSON.stringify(data);
+        return msg.replaceAll("|", this.lazy_sep);
+    };
 
     send(name, args, fun = console.log) {
-        const id = ++this.id
-        const self = this
+        const id = ++this.id;
+        const self = this;
 
         const message = this.javascriptToMessage({
-            'c': name,
-            'id' :id,
-            'a': args
-        })
+            "c": name,
+            "id" :id,
+            "a": args
+        });
 
-        if(message.includes('|')) {
-            alert('You can\'t use | characters!')
+        if(message.includes("|")) {
+            alert("You can\"t use | characters!");
 
-            return false
+            return false;
         }
 
         let callback = new Promise((resolve, reject) => {
             if(!self.callbacks[id]) {
-                self.callbacks[id] = {}
+                self.callbacks[id] = {};
             }
 
-            self.callbacks[id].resolve = resolve
-            self.callbacks[id].reject = reject
-            if(name === 'get' || name === 'on' || name === 'watch' || name === 'size' || name === 'sort') {
-                self.callbacks[id].sync = fun
+            self.callbacks[id].resolve = resolve;
+            self.callbacks[id].reject = reject;
+            if(name === "get" || name === "on" || name === "watch" || name === "size" || name === "sort") {
+                self.callbacks[id].sync = fun;
             }
         })
 
-        this.messageQueue.push(message)
+        this.messageQueue.push(message);
 
-        return callback
-    }
+        return callback;
+    };
 
-    forgot_password(email = '') {
-        return this.send('forgot_password', {
+    forgot_password(email = "") {
+        return this.send("forgot_password", {
             email
-        })
-    }
+        });
+    };
 
-    edit_password(password, uid = '') {
-        return this.send('edit_password', {
+    edit_password(password, uid = "") {
+        return this.send("edit_password", {
             password,
             uid
-        })
-    }
+        });
+    };
 
     connect(email, password) {
-        return this.send('connect', {
+        return this.send("connect", {
             email,
             password
-        })
-    }
+        });
+    };
 
-    register(email, password, username, full_name = '') {
-        return this.send('register', {
+    register(email, password, username, full_name = "") {
+        return this.send("register", {
             email,
             username,
             full_name,
             password
-        })
-    }
+        });
+    };
 
     create(keyPath, value = {}, w = true) {
-        keyPath = keyPath.split('/')
+        keyPath = keyPath.split("/");
 
-        return this.send('create', {
+        return this.send("create", {
             keyPath,
             value,
             w
-        })
-    }
+        });
+    };
 
     append(keyPath, value = {}) {
-        keyPath = keyPath.split('/')
+        keyPath = keyPath.split("/");
 
-        return this.send('append', {
+        return this.send("append", {
             keyPath,
             value
-        })
-    }
+        });
+    };
 
     on(command, keyPath) {
         return {
             then: async fn => {
-                keyPath = keyPath.split('/')
+                keyPath = keyPath.split("/");
 
-                return await this.send('on', {
+                return await this.send("on", {
                     keyPath,
                     command
-                }, fn)
+                }, fn);
             }
         }
-    }
+    };
 
     watch(command, keyPath) {
         return {
             then: async fn => {
-                keyPath = keyPath.split('/')
+                keyPath = keyPath.split("/");
 
-                return this.send('watch', {
+                return this.send("watch", {
                     keyPath,
                     command
-                }, fn)
+                }, fn);
             }
         }
-    }
+    };
 
     ping(command, keyPath) {
         return {
             then: async fn => {
-                keyPath = keyPath.split('/')
+                keyPath = keyPath.split("/");
 
-                return await this.send('on', {
+                return await this.send("on", {
                     keyPath,
                     command
-                }, fn)
+                }, fn);
             }
         }
-    }
+    };
 
     stop(event, command, keyPath) {
-        keyPath = keyPath.split('/')
+        keyPath = keyPath.split("/");
 
-        return this.send('stop', {
+        return this.send("stop", {
             event,
             command,
             keyPath
-        })
-    }
+        });
+    };
 
     size(keyPath) {
         return {
             then: async fn => {
-                keyPath = keyPath.split('/')
+                keyPath = keyPath.split("/");
 
-                return await this.send('size', {
+                return await this.send("size", {
                     keyPath
-                }, fn)
+                }, fn);
             }
         }
-    }
+    };
 
-    sort(keyPath, split = {char: '_', num: 1}, result = {count: 10, start: 0, order: 'asc'}, order = 'asc') {
+    sort(keyPath, split = {char: "_", num: 1}, result = {count: 10, start: 0, order: "asc"}, order = "asc") {
         return {
             then: async fn => {
-                keyPath = keyPath.split('/')
+                keyPath = keyPath.split("/");
 
-                return await this.send('sort', {
+                return await this.send("sort", {
                     keyPath,
                     split,
                     result,
                     order
-                }, fn)
+                }, fn);
             }
         }
-    }
+    };
 
     get(keyPath, depth = 99) {
-        keyPath = keyPath.split('/')
+        keyPath = keyPath.split("/");
 
-        return this.send('get', {
+        return this.send("get", {
             keyPath,
             depth
-        })
-    }
+        });
+    };
 
     exist(keyPath) {
-        keyPath = keyPath.split('/')
+        keyPath = keyPath.split("/");
 
-        return this.send('exist', {
+        return this.send("exist", {
             keyPath
-        })
-    }
+        });
+    };
 
     update(keyPath, value, w = true) {
-        keyPath = keyPath.split('/')
+        keyPath = keyPath.split("/");
 
-        return this.send('update', {
+        return this.send("update", {
             keyPath,
             value,
             w
-        })
-    }
+        });
+    };
 
     delete(keyPath) {
-        keyPath = keyPath.split('/')
+        keyPath = keyPath.split("/");
 
-        return this.send('delete', {
+        return this.send("delete", {
             keyPath
-        })
-    }
+        });
+    };
 
-    keys(keyPath, filter = 'all') {
-        keyPath = keyPath.split('/')
+    keys(keyPath, filter = "all") {
+        keyPath = keyPath.split("/");
 
-        return this.send('keys', {
+        return this.send("keys", {
             keyPath,
             filter
-        })
-    }
+        });
+    };
 }
